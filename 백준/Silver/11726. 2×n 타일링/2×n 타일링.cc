9f94d4878917a878ae2e956558edd67fddebb4aa
Sohee Kim <wneh2023@naver.com>
2026-03-05 19:51:03 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,18 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, dp[1004];
-int solve(int num) {
-    int& ret = dp[num];
-    if (ret != -1) return ret;
-    ret = 0;
-    ret += solve(num-1) + solve(num-2);
-    return ret %= 10007;
-}
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr);
 	cin >> n;
-    fill(dp, dp+1004, -1);
-    dp[1] = 1; dp[2] = 2;
-    cout << solve(n) << "\n";
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+    }
+
+    cout << dp[n] % 10007 << "\n";
 }
