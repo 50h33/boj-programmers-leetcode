cdc8e1ded01cdefcffc293fefff469f6b386131e
Sohee Kim <wneh2023@naver.com>
2026-03-05 17:03:37 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,21 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[1000001];
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    int n;
-    cin >> n;
-    dp[1] = 0;
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i] = dp[i - 1] + 1;
-        if (i % 2 == 0)
-            dp[i] = min(dp[i], dp[i / 2] + 1);
-        if (i % 3 == 0)
-            dp[i] = min(dp[i], dp[i / 3] + 1);
-    }
-    cout << dp[n];
+const int INF = 1e9;
+int n;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n;
+	int dp[n+4];
+	fill(dp, dp + n + 4, INF);
+	dp[1] = 0;
+	for (int i = 2; i <= n; i++) {
+		if (i % 3 == 0) dp[i] = min(dp[i], dp[i/3] + 1);
+		if (i % 2 == 0) dp[i] = min(dp[i], dp[i/2] + 1);
+		dp[i] = min(dp[i], dp[i-1] + 1);
+	}
+	cout << dp[n] << "\n";
 }
