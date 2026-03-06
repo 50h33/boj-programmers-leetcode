cb872a9ca8594a01832d9313d9a304be64841186
Sohee Kim <wneh2023@naver.com>
2026-03-06 18:43:12 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 4 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.cc
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.cc
@@ -1,36 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int v[1000001];
-int d[50001][101];
-int s, k;
-
-int dp(int x, int count, int length) {
-	if (x <= 0 || count > k) return length;
-	if (d[x][count] != 0) return d[x][count];
-
-	int value = 0;
-	if (v[x] % 2 == 0) value = dp(x - 1, count, length + 1);
-	else if (count < k) value = dp(x - 1, count + 1, length);
-	else if (count == k) return length;
-;	return d[x][count] = value;
-}
+int n, k, a[50004], l, odd, ans;
 int main() {
-	ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-	cin >> s >> k;
-
-	for (int i = 1; i <= s; i++) {
-		cin >> v[i];
-	}
-
-	int ans = 0;
-
-	for (int i = s; i >= 1; i--) {
-        for (int j = 0; j <= k; j++) {
-			if (v[i] % 2 == 0) ans = max(ans, dp(i, j, 0));
-		}
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
     
-	cout << ans;
+    for (int r = 0; r < n; r++) {
+        if (a[r] % 2 == 1) odd++;
+
+        while (odd > k) {
+            if (a[l] % 2 == 1) odd--;
+            l++;
+        }
+
+        ans = max(ans, r - l - odd + 1);
+    }
+
+    cout << ans << "\n";
 }
